dd7719c3b0fe4776b0e34bf30ebfa9a5e436bcd1
arzzen <lukas.mestan@gmail.com>
2025-06-16 14:03:21 +0200

    sorting contribution stats

--- tests/commands_test.sh
+++ tests/commands_test.sh
@@ -11,7 +11,7 @@
 . tests/assert.sh -v
 
 src="./git-quick-stats"
-assert "$src fail" "Invalid argument
+assert "$src fail" "Invalid argument: fail
 
 NAME
     git-quick-stats - Simple and efficient way to access various stats in a git repo
@@ -23,9 +23,8 @@ SYNOPSIS
 DESCRIPTION
     Any git repository contains tons of information about commits, contributors,
     and files. Extracting this information is not always trivial, mostly because
-    of a gadzillion options to a gadzillion git commands.
-
-    This program allows you to see detailed information about a git repository.
+    of a gadzillion options to a gadzillion git commands. This program allows
+    you to see detailed information about a git repository.
 
 GENERATE OPTIONS
     -T, --detailed-git-stats
@@ -81,11 +80,11 @@ SUGGEST OPTIONS
 
 ADDITIONAL USAGE
     You can set _GIT_SINCE and _GIT_UNTIL to limit the git time log
-        ex: export _GIT_SINCE=\"2017-01-20\"
+        ex: export _GIT_SINCE="2017-01-20"
     You can set _GIT_LIMIT for limited output log
         ex: export _GIT_LIMIT=20
     You can set _GIT_LOG_OPTIONS for git log options
-        ex: export _GIT_LOG_OPTIONS=\"--ignore-all-space --ignore-blank-lines\"
+        ex: export _GIT_LOG_OPTIONS="--ignore-all-space --ignore-blank-lines"
     You can exclude directories or files from the stats by using pathspec
         ex: export _GIT_PATHSPEC=':!pattern'
     You can set _GIT_MERGE_VIEW to view merge commits with normal commits
@@ -97,14 +96,16 @@ ADDITIONAL USAGE
     You can set _GIT_BRANCH to set the branch of the stats
         ex: export _GIT_BRANCH=master
     You can set _GIT_IGNORE_AUTHORS to filter out specific authors
-        ex: export _GIT_IGNORE_AUTHORS=\"(author1|author2)\""
+        ex: export _GIT_IGNORE_AUTHORS=\"(author1|author2)\"
+    You can sort contribution stats by field name, commits, insertions, deletions, or lines - total lines changed and order - asc, desc. e.g.: commits-desc
+        ex: export _GIT_SORT_BY=name-asc"
 
 assert_raises "$src fail" 1
 
 assert_contains "$src --suggest-reviewers" "Suggested code reviewers (based on git history)"
 assert_success "$src --suggest-reviewers"
 
-assert_contains "$src --detailed-git-stats" "Contribution stats"
+assert_contains "$src --detailed-git-stats" "Contribution stats (by author) on the current branch"
 assert_success "$src --detailed-git-stats"
 
 assert_contains "$src --commits-per-day" "Git commits per date"
